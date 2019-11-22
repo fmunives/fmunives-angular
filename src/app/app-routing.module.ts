3b73b1a7c655c4b53431379bd3993cf1815496c4
Freddy Munive <munivef@yahoo.com>
2019-11-22 18:46:18 -0500

    agregando plantilla a proyecto angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { KnowledgeComponent } from './components/knowledge/knowledge.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { AwardsComponent } from './components/awards/awards.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AboutComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'knowledge', component: KnowledgeComponent },
+  { path: 'projects', component: ProyectsComponent },
+  { path: 'awards', component: AwardsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { KnowledgeComponent } from './components/knowledge/knowledge.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { AwardsComponent } from './components/awards/awards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    KnowledgeComponent,
+    ProyectsComponent,
+    AwardsComponent
   ],
   imports: [
     BrowserModule,
